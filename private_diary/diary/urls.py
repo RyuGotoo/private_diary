6d2fb4de134c84d2b98eba48e834a3794ce2ead4
RyuGotoo <ryu.gotoo@gmail.com>
2022-12-27 12:50:58 +0000

    feature: diaryのurlとviews

--- private_diary/diary/urls.py
+++ private_diary/diary/urls.py
@@ -6,4 +6,5 @@ app_name = "diary"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("inquiry/", views.InquiryView.as_view(), name="inquiry"),
+    path("diary-list/", views.DiaryListView.as_view(), name="diary_list"),
 ]
--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -3,8 +3,10 @@ import logging
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import InquiryForm
+from .models import Diary
 
 logger = logging.getLogger(__name__)
 
@@ -23,3 +25,12 @@ class InquiryView(generic.FormView):
         messages.success(self.request, "メッセージを送信しました。")
         logger.info("Inquiry sent by {}".format(form.cleaned_data["name"]))
         return super().form_valid(form)
+
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = "diary_list.html"
+
+    def get_queryset(self):
+        diaries = Diary.objects.filter(user=self.request.user).order_by("-created_at")
+        return diaries
