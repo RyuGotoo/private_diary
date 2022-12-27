ba277773b79790c274c8b34a4f80af1bf391eeca
RyuGotoo <ryu.gotoo@gmail.com>
2022-12-27 13:06:28 +0000

    fix typo

--- private_diary/private_diary/settings_common.py
+++ private_diary/private_diary/settings_common.py
@@ -171,6 +171,6 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 # デフォルトのメール送信元を設定
 DEFAULT_FROM_EMAIL = os.environ.get("FROM_EMAIL")
 
-MEDIA_URL = "/media"
+MEDIA_URL = "/media/"
 
 LOGIN_REDIRECT_URL = "diary:diary_list"
