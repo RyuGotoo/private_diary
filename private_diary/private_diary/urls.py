dfa1dc88c213ab934d0d8c608319482d94e69b4c
RyuGotoo <ryu.gotoo@gmail.com>
2022-12-27 10:10:42 +0000

    feature: allauthを使うための設定

--- private_diary/private_diary/urls.py
+++ private_diary/private_diary/urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("diary.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("diary.urls")),
+    path("accounts/", include("allauth.urls")),
+]
