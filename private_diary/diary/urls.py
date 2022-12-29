8d1c930ef1a7432d53cc717691b4278ce64a968e
RyuGotoo <ryu.gotoo@gmail.com>
2022-12-29 00:58:41 +0000

    feature: 日記編集機能実装

--- private_diary/diary/urls.py
+++ private_diary/diary/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("diary-list/", views.DiaryListView.as_view(), name="diary_list"),
     path("diary-detail/<int:pk>", views.DiaryDetailView.as_view(), name="diary_detail"),
     path("diary-create/", views.DiaryCreateView.as_view(), name="diary_create"),
+    path(
+        "diary-update/<int:pk>/", views.DiaryUpdateView.as_view(), name="diary_update"
+    ),
 ]
--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -58,3 +58,20 @@ class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
     def form_invalid(self, form: DiaryCreateForm):
         messages.error(self.request, "日記の作成に失敗しました。")
         return super().form_invalid(form)
+
+
+class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Diary
+    template_name = "diary_update.html"
+    form_class = DiaryCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy("diary:diary_detail", kwargs={"pk": self.kwargs["pk"]})
+
+    def form_valid(self, form: DiaryCreateForm):
+        messages.success(self.request, "日記を更新しました。")
+        return super().form_valid(form)
+
+    def form_invalid(self, form: DiaryCreateForm):
+        messages.error(self.request, "日記の更新に失敗しました。")
+        return super().form_invalid(form)
