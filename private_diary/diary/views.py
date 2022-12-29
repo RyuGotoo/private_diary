0c7453d766669ea9117f417ee208039e17777d5d
RyuGotoo <ryu.gotoo@gmail.com>
2022-12-29 02:35:28 +0000

    feature: 日記削除機能実装

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -75,3 +75,13 @@ class DiaryUpdateView(LoginRequiredMixin, generic.UpdateView):
     def form_invalid(self, form: DiaryCreateForm):
         messages.error(self.request, "日記の更新に失敗しました。")
         return super().form_invalid(form)
+
+
+class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Diary
+    template_name = "diary_delete.html"
+    success_url = reverse_lazy("diary:diary_list")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "日記を削除しました。")
+        return super().delete(request, *args, **kwargs)
